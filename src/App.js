bcdb6dcb53d592d0eb6bf38e574b7a8d3e7d5d70
karthi3232 <karthi323287@gmail.com>
2021-06-22 14:49:46 +0530

    FortuneSoft First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { HomePage } from './Pages/HomePage/HomePage';
+import { DetailsPage } from './Pages/DetailsPage/DetailsPage';
+import { createBrowserHistory } from "history";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
+
+const customHistory = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={customHistory}>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/:movieSlug" component={DetailsPage}/>
+      </Switch>
+    </Router>
   );
 }
 
