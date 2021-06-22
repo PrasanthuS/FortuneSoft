086c0e090d7385e55ed9d7ca9de8bbaf9d467205
karthi3232 <karthi323287@gmail.com>
2021-06-22 15:07:13 +0530

    Modified Code

--- src/network.js
+++ src/network.js
@@ -8,22 +8,13 @@ class Network {
             headers: {'Authorization': 'Bearer Wookie2019'}
           });
     }
-
     getAllMovies(){
         return this.instance.get(`/movies`)
     }
-
     searchMovie(q){
         return this.instance.get(`/movies?q=${q}`)
     }
-
 }
 
-
-
-/**
- * This exports a singleton. The Network can be imported to any component will be the same instance
- *  Allows for synchoronized network calls, authentication, access tokens, etc.
- */
 const singleton = new Network();
 export default singleton;
